4eaffc0a28f209fe7d9bc6f61148b274d5666b3f
Andrew Edwards <ae0000@gmail.com>
2015-01-15 10:10:34 +1100

    updating for new api

--- tests/example.php
+++ tests/example.php
@@ -2,24 +2,75 @@
 require_once(__DIR__.'/../src/Attendly.php');
 
 
-$attendly = new Attendly;
+$attendly = new Attendly('ae00001','1lIH8Fq8szwvMZg6A1WU');
+$attendly->server = 'https://api.attendly.net';
 
-// Login (you should only do this to get the apikey once)
-$user = $attendly->user_login('bob','password');
-// Store the apikey
-$attendly->apikey = $user['user']['apikey'];
 
-// Get my list of events
-$events = $attendly->event_list();
+// Create an event
+$event = array(
+		'Name' => 'Test event via API',
+		'Description'=> '<p>This is just a test event. Note <strong>HTML</strong>strong></p>p>',
+		'Deadline'=> '2015-11-07T23:00:00Z',
+		'Start'=> '2015-11-10T23:00:00Z',
+		'Stop'=> '2015-11-10T23:00:00Z',
+		'Status'=> 'active'
+);
 
-foreach ($events as $event)
-{
-	echo $event['event']['url']."\n";
-}
+$ticket_1 = array(
+		'Name' => 'Test ticket number one',
+		'Description' => 'General',
+		'Price' => 0,
+		'Total' => 1000,
+		'Order' => 1,
+		'Status' => 'active'
+);
 
-// Get an events total ticket sales ($)
-if ( ! empty($events))
-{
-	$event = $attendly->event_get($events[0]['event']['id']);
-	echo $event['event']['gross_sales'];
-}
+$ticket_2 = array(
+		'Name' => 'Test ticket number two',
+		'Description' => 'General',
+		'Price' => 0,
+		'Total' => 1000,
+		'Order' => 2,
+		'Status' => 'active'
+);
+
+$text_widget = array(
+		'Name' => 'Test widget one',
+		'Description' => 'Just a widget',
+		'Label' => 'A test label',
+		'Order' => 1,
+		'Type' => 'text',
+		'Status' => 'active'
+);
+
+$textarea_widget = array(
+		'Name' => 'Test widget two',
+		'Description' => 'General',
+		'label' => 'A test textarea',
+		'Order' => 2,
+		'Type' => 'textarea',
+		'Status' => 'active'
+);
+
+$address = array(
+	'Name' => 'Test address',
+	'Line1' => '60 Symes Road',
+	'City' => 'Muckleford South',
+	'Post' => '3462',
+	'State' => 'VIICTORIA',
+	'Country' => 'Australia',
+	'Status' => 'active'
+);
+
+$result = $attendly
+	->add_event($event)
+	->add_ticket($ticket_1)
+	->add_ticket($ticket_2)
+	->add_ticket_limit(2000)
+	->add_widget($text_widget)
+	->add_widget($textarea_widget)
+	->add_address($address)
+	->event_create();
+
+
+var_dump($result);
